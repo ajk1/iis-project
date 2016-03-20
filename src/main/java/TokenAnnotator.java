6d359a4e4aa6f7369d9b847d88eb60fb0938129f
terry81811 <terrytsai0811@gmail.com>
2016-03-20 17:28:48 -0400

    disable type doc for ngramAnnotator and sentiTokenAnnotator

--- src/main/java/TokenAnnotator.java
+++ src/main/java/TokenAnnotator.java
@@ -36,51 +36,42 @@ public class TokenAnnotator extends JCasAnnotator_ImplBase {
 		sizeLimit = Integer.valueOf((String) aContext.getConfigParameterValue(PARAM_SIZELIMIT));
 	}
 
+	@Override
+	public void process(JCas aJCas) throws AnalysisEngineProcessException {
+		System.out.println(">> Token Annotator Processing");
+		System.out.println("... sizeLimit : " + sizeLimit);
+    
+	    //component implementation	          
+	    StopWordUtils swu = new StopWordUtils("limit");
 	
-  @Override
-  public void process(JCas aJCas) throws AnalysisEngineProcessException {
-    System.out.println(">> Token Annotator Processing");
-    System.out.println("... sizeLimit : " + sizeLimit);
-    
-    StopWordUtils swu = new StopWordUtils("limit");
-
-    // get document text from the CAS
-    String docText = aJCas.getDocumentText();
-
-    FSIterator reviewIterator = aJCas.getAnnotationIndex(Review.type).iterator();
-    
-    Collection<Review> reviews = JCasUtil.select(aJCas, Review.class);
-      
-    System.out.println("... review size: " + reviews.size());
-
-    int ctr = 0;
-    //TODO: get review list from cas, not inputDoc
-    for (Review review : reviews) {
-    	if(ctr++ > sizeLimit) break;
-    	  
-        String body = review.getRawText();
-          
-        //component implementation
-          
-        List<type.Sentence> sentences = new ArrayList<type.Sentence>();
-          
-        //annotate sentences
-        Reader reader = new StringReader(body);
-        DocumentPreprocessor dp = new DocumentPreprocessor(reader);
-        for(List<HasWord> hw: dp) {
-        	String rawSentenceText = Sentence.listToString(hw);
-        	String cleanSentenceText = swu.removeStopword(rawSentenceText);
-        	  
-            type.Sentence sentence = new type.Sentence(aJCas);
-            sentence.setRawText(cleanSentenceText);
-            sentence.addToIndexes();              
-            sentences.add(sentence);
-        }
-        review.setSentences(Utils.fromCollectionToFSList(aJCas, sentences));	//add sentences to review scope list
-    	System.out.println("... review: " + ctr + " added "+ sentences.size() +" sentences");
-          
-      	}
-    }
+	    // get reviews from the CAS
+	    Collection<Review> reviews = JCasUtil.select(aJCas, Review.class);      
+	    System.out.println("... review size: " + reviews.size());
+	
+	    int ctr = 0;
+	    for (Review review : reviews) {
+	    	if(ctr++ > sizeLimit) break;
+	    	  
+	        String body = review.getRawText();
+	          
+	        List<type.Sentence> sentences = new ArrayList<type.Sentence>();
+	          
+	        //annotate sentences
+	        Reader reader = new StringReader(body);
+	        DocumentPreprocessor dp = new DocumentPreprocessor(reader);
+	        for(List<HasWord> hw: dp) {
+	        	String rawSentenceText = Sentence.listToString(hw);
+	        	String cleanSentenceText = swu.removeStopword(rawSentenceText);
+	        	  
+	            type.Sentence sentence = new type.Sentence(aJCas);
+	            sentence.setRawText(cleanSentenceText);
+	            sentence.addToIndexes();              
+	            sentences.add(sentence);
+	        }
+	        review.setSentences(Utils.fromCollectionToFSList(aJCas, sentences));	//add sentences to review scope list
+	    	System.out.println("... review: " + ctr + " added "+ sentences.size() +" sentences");
+	    }
+	}
     
  
 }
